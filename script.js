8ad374df349e41d0eedfaaa3de677205165689a7
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-07-07 13:46:30 +0200

    Add element variable and render it on div with "app" id

--- script.js
+++ script.js
@@ -18,8 +18,16 @@ var Counter = React.createClass({
     },
 
     render: function() {
-        return React.createElement('div', {onClick: this.increment},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        return React.createElement('div', {},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter),
+            React.createElement('button', {onClick: this.increment}, 'Dodaj 1'),
+            React.createElement('button', {onClick: this.decrement}, 'Odejmij 1')
         );
     }
 });
+
+var element = React.createElement('div', {},
+    React.createElement(Counter, {})
+    );
+
+ReactDOM.render(element, document.getElementById('app'));
